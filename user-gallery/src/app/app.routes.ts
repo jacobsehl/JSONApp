9e7eaaaa9788ba222189fea15e00d9527d41597e
jacobsehl <jacobsehl@gmail.com>
2025-05-23 01:54:14 -0400

    Added Styling for Home Page and User List

--- user-gallery/src/app/app.routes.ts
+++ user-gallery/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { AlbumListComponent } from './components/album-list/album-list.component';
 import { PhotoListComponent } from './components/photo-list/photo-list.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'users', component: UserListComponent },
   { path: 'users/:id/albums', component: AlbumListComponent },
   { path: 'albums/:id/photos', component: PhotoListComponent },
