7364fa50e18aba318927df30165bb7b3059a2752
jacobsehl <jacobsehl@gmail.com>
2025-05-23 02:23:04 -0400

    Updates to albums and users styling
    Having to use random placeholders because the placeholder.com is down. Annoying but alas.

--- user-gallery/src/app/components/album-list/album-list.component.ts
+++ user-gallery/src/app/components/album-list/album-list.component.ts
@@ -18,10 +18,23 @@ export class AlbumListComponent implements OnInit {
   constructor(private route: ActivatedRoute, private api: ApiService) {}
 
   ngOnInit(): void {
+
     this.userId = Number(this.route.snapshot.paramMap.get('id'));
+
     this.api.getAlbums(this.userId).subscribe({
-      next: (data) => {
-        this.albums = data;
+      next: async (albums) => {
+        const albumsWithThumbnails = await Promise.all(
+          albums.map(async (album: any) => {
+            const photos: any[] = await this.api.getPhotos(album.id).toPromise();
+            return {
+              ...album,
+              // thumbnailUrl: photos[0]?.thumbnailUrl || null
+              // placeholder.com is down on 05/23/25 so we are generating random image instead.
+              thumbnailUrl: `https://picsum.photos/seed/${album.id}/150/150`
+            };
+          })
+        );
+        this.albums = albumsWithThumbnails;
         this.loading = false;
       },
       error: () => {
