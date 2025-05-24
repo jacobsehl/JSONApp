672775ea74b404b00abfc38e1ac8584806dcd6df
jacobsehl <jacobsehl@gmail.com>
2025-05-23 20:54:43 -0400

    Updated Frontend Styling

--- user-gallery/src/app/components/photo-list/photo-list.component.ts
+++ user-gallery/src/app/components/photo-list/photo-list.component.ts
@@ -23,8 +23,12 @@ export class PhotoListComponent implements OnInit {
     this.albumId = Number(this.route.snapshot.paramMap.get('id'));
     this.api.getPhotos(this.albumId).subscribe({
       next: (data) => {
-        this.photos = data;
-        this.loading = false;
+      this.photos = data.map((photo: { id: any; }) => ({
+        ...photo,
+        // we are mapping over and reassinging thumbnails to a random image 
+        // because placeholder.com is down
+        thumbnailUrl: `https://picsum.photos/seed/${photo.id}/150/150`
+      }));        this.loading = false;
       },
       error: () => {
         this.error = 'Failed to Load Photos';
